e3798450505a6796a5934d0fdb0b4d91c457c553
root <root@ubuntu-cinf301.localdomain>
2017-03-20 01:50:04 +0000

    Commented index.php and connect.php, too afraid to touch .sql

--- connect.php
+++ connect.php
@@ -1,14 +1,22 @@
 <?php
-$dsn = 'mysql:dbname=project3_partA;host=0.0.0.0';
-$user= 'newUser';
-$pass = 'n3wus3rp4ssw0rd';
-$pdo = new PDO($dsn,$user,$pass);
-function getAll($db)
-{
-	return$db->query('SELECT * FROM Cats WHERE id =' . $_GET['id']);
-}
-foreach(getAll($pdo) as $row)
-{
-	echo "<li>".$row['name']." - ".$row['cuteness']."</li>";
-}
+/*
+	PHP Code for PDO Database access. The first few variables are for initializing the PDO access. After that the function getInfo queries the database for the primary key id equal to the one given in the form in index.php. The foreach loop then displays the information.
+
+	Author:TierneyIrwin
+	Date:3/19/17
+	Version:1.0
+
+*/
+	$dsn = 'mysql:dbname=project3_partA;host=0.0.0.0';
+	$user= 'newUser';
+	$pass = 'n3wus3rp4ssw0rd';
+	$pdo = new PDO($dsn,$user,$pass);
+	function get($db)
+	{
+		return$db->query('SELECT * FROM Cats WHERE id =' . $_GET['id']);
+	}
+	foreach(get($pdo) as $row)
+	{
+		echo "<li>".$row['name']." - ".$row['cuteness']."</li>";
+	}
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
+<!-- 
+	Here is the HTML code for the form of the page. Nothing crazy
+	just a simple form that gets inputted once you enter information and 
+	press Enter. Here we are inputting integers to find the primary keys 
+	in the database.
+	Author:TierneyIrwin
+	Date:3/19/17
+	Version:1.0
+-->
 	<form method="get" action="connect.php">
 		<input type="number" name="id">
 	</form>
